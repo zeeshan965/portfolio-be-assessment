5f87b8a99539ebeb34eb81b9886972f700b16301
zeeshan <zeeshanbutt223@gmail.com>
2024-05-25 11:11:41 +0500

    backing up code

--- src/entities/PageEntity.ts
+++ src/entities/PageEntity.ts
@@ -1,14 +1,19 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, ObjectType } from 'type-graphql';
 import PortfolioVersionEntity from './PortfolioVersionEntity';
 
+@ObjectType('PageEntity')
 @Entity()
 export default class PageEntity {
+  @Field()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column('varchar', { nullable: false })
   name: string;
 
+  @Field()
   @Column('varchar', { nullable: false, unique: true })
   url: string;
 
--- src/resolvers/PortfolioVersion.resolver.ts
+++ src/resolvers/PortfolioVersion.resolver.ts
@@ -1,9 +1,11 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, Query, Resolver } from 'type-graphql';
 import { getRepository } from 'typeorm';
+import { Service } from 'typedi';
 import PortfolioVersionEntity from '../entities/PortfolioVersionEntity';
 import PageEntity from '../entities/PageEntity';
 
 @Resolver()
+@Service()
 export default class PortfolioVersionResolver {
   @Query(() => [PortfolioVersionEntity])
   async portfolioVersions(): Promise<PortfolioVersionEntity[]> {
@@ -15,35 +17,5 @@ export default class PortfolioVersionResolver {
   async portfolioPages(@Arg('versionId') versionId: number): Promise<PageEntity[]> {
     const pageRepository = getRepository(PageEntity);
     return pageRepository.find({ where: { version: { id: versionId } } });
-  }
-
-  @Mutation(() => PortfolioVersionEntity)
-  async createSnapshotVersion(@Arg('draftVersionId') draftVersionId: number): Promise<PortfolioVersionEntity> {
-    const portfolioVersionRepository = getRepository(PortfolioVersionEntity);
-    const pageRepository = getRepository(PageEntity);
-
-    const draftVersion = await portfolioVersionRepository.findOne(draftVersionId, { relations: ['portfolio', 'pages'] });
-    if (!draftVersion) {
-      throw new Error('Draft version not found');
-    }
-
-    const snapshotVersion = portfolioVersionRepository.create({
-      portfolio: draftVersion.portfolio,
-      versionType: 'snapshot',
-      pages: []
-    });
-
-    await portfolioVersionRepository.save(snapshotVersion);
-
-    for (const page of draftVersion.pages) {
-      const newPage = pageRepository.create({
-        name: page.name,
-        url: page.url,
-        version: snapshotVersion
-      });
-      await pageRepository.save(newPage);
-    }
-
-    return snapshotVersion;
   }
 }
